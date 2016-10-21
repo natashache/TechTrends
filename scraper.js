516ae065e1d2e713238f909e3e4fb15b4b724232
Numose <andrew.cernek@gmail.com>
2016-10-21 13:24:21 -0700

    added getSource method to keys, made scraper do one query at a time instead of batching all

--- scraper.js
+++ scraper.js
@@ -5,60 +5,55 @@ const cheerio = require('cheerio');
 const keysMethods = require('./services/keys.js');
 const promise = require('bluebird');
 
-const queries = keysMethods.getQueries();
-
 // TODO: need recursive call for all pages; remove id
-const fetchRecordUrls = function(queries) {
+const fetchRecordUrls = function(query) {
   return new Promise(function(resolve, reject) {
 
     var records = [];
-
-    queries.forEach(function(query) {
       
-      const parseUrls = function(url) {
-        request.get(url, function(err, response, html) {
-          if (!err) {
-            
-            const $ = cheerio.load(html);
-
-            const urls = $('body').find('h2.job-title a'); // TODO: abstract this into source utility
-            
-            Object.keys(urls).forEach(function(listing) {
-              if (urls[listing].attribs !== undefined) {
-                
-                var record = {
-                  id: '', // TODO: remove?
-                  date: query.date,
-                  country: query.country,
-                  state: query.state,
-                  hub: query.hub,
-                  source: query.source,
-                  term: query.term,
-                  url: '',
-                  text: ''
-                };
-                
-                record.url = urls[listing].attribs.href;
-                record.url = 'http://www.careerbuilder.com' + record.url.split('?')[0]; // TODO: abstract this into source utility
-                record.id = Math.random() * 100000000000000000000;  // TODO: remove
-                
-                records.push(record);
-              }
-            });
-
-            // TODO: recursive logic here to check for additional pages and feed them back into this fetch until all are listed
-
-            resolve(records);
+    const parseUrls = function(url) {
+      request.get(url, function(err, response, html) {
+        if (!err) {
           
-          } else {
-            reject(console.log(err));
-          }
-        });
-      };
+          const $ = cheerio.load(html);
 
-      parseUrls(query.start);
-    
-    });
+          const urls = $('body').find('h2.job-title a'); // TODO: abstract this into source utility
+          
+          Object.keys(urls).forEach(function(listing) {
+            if (urls[listing].attribs !== undefined) {
+              
+              var record = {
+                id: '', // TODO: remove?
+                date: query.date,
+                country: query.country,
+                state: query.state,
+                hub: query.hub,
+                source: query.source,
+                term: query.term,
+                url: '',
+                text: ''
+              };
+              
+              record.url = urls[listing].attribs.href;
+              record.url = 'http://www.careerbuilder.com' + record.url.split('?')[0]; // TODO: abstract this into source utility
+              record.id = Math.random() * 100000000000000000000;  // TODO: remove
+              
+              records.push(record);
+            }
+          });
+
+          // TODO: recursive logic here to check for additional pages and feed them back into this fetch until all are listed
+
+          resolve(records);
+        
+        } else {
+          reject(console.log(err));
+        }
+      });
+    };
+
+    parseUrls(query.start);
+
   });
 };
 
@@ -98,15 +93,17 @@ const fetchRecordContent = function(records) {
 
 // TODO: replace with write to DB
 const storeRecords = function(records) {
-
   records.forEach(function(record) {
     fs.writeFile((__dirname + '/services/records/' + record.id + '.txt'), JSON.stringify(record), function(err) {
       if (err) console.log(err);
     });
   });
-
 };
 
-fetchRecordUrls(queries)
-  .then(fetchRecordContent)
-  .then(storeRecords);
+const queries = keysMethods.getQueries();
+
+queries.forEach(function(query) {
+  fetchRecordUrls(query)
+    .then(fetchRecordContent)
+    .then(storeRecords);
+});
