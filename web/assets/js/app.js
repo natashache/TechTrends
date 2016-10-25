e11315e5fe4648d5e074ee7b166234143dde8c53
Aaron Stevens <astev1989@gmail.com>
2016-10-25 17:42:31 -0400

    states dropdown rendering correctly

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -1,5 +1,11 @@
 angular.module('mainApp', [
 
-])
+]).controller('stateCtrl', ['$scope', function($scope) {
+  $scope.data = {
+    default: 'Select State:',
+    states: Object.keys(keys.collections.geo.united_states)
+  }
+
+}])
 
 
