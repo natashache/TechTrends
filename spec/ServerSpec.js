f6499361dc72e521490e407186155486758f70ee
Justin <jproche5@gmail.com>
2016-10-25 00:25:59 -0400

    server tests for existing endpoints

--- spec/ServerSpec.js
+++ spec/ServerSpec.js
@@ -4,6 +4,8 @@ var rp = require('request-promise');
 var server = 'http://127.0.0.1:8000';
 var chai = require('chai');
 var expect = chai.expect;
+var promise = require('bluebird');
+var bodyparser = require('body-parser');
 
 var examplePosting1 = {
   "date": "1000", 
@@ -33,10 +35,45 @@ var examplePosting3 = {
   "source": "career_builder", 
   "url": "www.fff.com", 
   "text": "some posting text"
+  };
+
+var examplePosting4 = {
+  "date": "1001", 
+  "country": "D", 
+  "state": "Maryland", 
+  "hub": "la", 
+  "source": "career_builder", 
+  "url": "www.fff.com", 
+  "text": "some posting text"
   }; 
 
+
+var postone = function(){
+  return rp.post(server+'/raw-postings',{json: examplePosting1});
+}
+
+var posttwo= function(){
+  return rp.post(server+'/raw-postings',{json: examplePosting2});
+}
+
+var postfour = function(){
+  return rp.post(server+'/raw-postings',{json: examplePosting4});
+}
+
+var postthree =  function(){
+    return promise.all([rp.post(server+'/raw-postings',{json: examplePosting1}),
+                        rp.post(server+'/raw-postings',{json: examplePosting2}),
+                        rp.post(server+'/raw-postings',{json: examplePosting3}),]);
+  };
+
+var deleteall = function(){return rp.delete(server+'/raw-postings/:0');};
+var deleteone = function(){return rp.delete(server+'/raw-postings/:1001');};
+
+var getall = function(){return rp.get(server+'/raw-postings?date=0');};
+var getone = function(){return rp.get(server+'/raw-postings?date=1001');};
+
 beforeEach(function(done){
-  rp.delete(server+'/raw-postings/:0').then(res=>{
+  deleteall().then(res=>{
     done();
   });
 })
@@ -46,122 +83,157 @@ describe ('service of static assets',function(){
     request(server+'/').on('response',function(res){
       expect(res.statusCode).to.equal(200);
       done();
-   });
+    })
   });
 });
 
-describe ('raw-postings api',function(){
-  it('shows nothing after deletion',function(done){
-    //rp.post(server+'/raw-postings',{json: examplePosting1})
-    Promise.all([ rp.delete(server+'/raw-postings/:0'),
-                  rp.get(server+'/raw-postings'), 
-                  ])
-      .then(responses=>{
-        console.log('r1',responses);
-        expect(JSON.parse(responses[1]).length).to.equal(0);
-        done();
+describe('raw-postings post request',function(){
+  it('posts with statusCode of 202',function(done){
+      postone()
+        .on('response',response=>{
+          expect(response.statusCode).to.equal(202);
+          done();
+        });
     });
-  });
-
-  it('posts',function(done){
-    Promise.all([rp.post(server+'/raw-postings',{json: examplePosting1}),
-                 rp.post(server+'/raw-postings',{json: examplePosting2}),
-                 rp.post(server+'/raw-postings',{json: examplePosting3}),])
-      .then(responses=>{
-        expect(JSON.parse(responses[1]).country).to.equal('A');
-        done();
-      });
-  });
-
+    
   it('returns the posted object from a post request',function(done){
-    rp.post(server+'/raw-postings',{json: examplePosting1})
-      .then(response=>{
-        console.log('post response',response);
+      postone()
+        .then(response=>{
+          expect(response.country).to.equal('A');
+        })
+        .catch()
+        .then(done);
+    });
+
+});
+
+describe('raw-postings get request',function(){
+
+  it('gets postings with statusCode of 202',function(done){
+    getall()
+      .on('response',response=>{
+        expect(response.statusCode).to.equal(202);
         done();
       });
   });
 
-  it('gets the results of posts',function(done){
-    Promise.all([rp.post(server+'/raw-postings',{json: examplePosting1}),
-                 rp.post(server+'/raw-postings',{json: examplePosting2}),
-                 rp.post(server+'/raw-postings',{json: examplePosting3}),])
-      .then(rp.get(server+'/raw-postings?date=0'))
-      .then(res=>{
-        console.log('results of get', res);
-      });
+  it('getall returns an array of collections',function(done){
+    postone()
+      .then(getall)
+      .then(response=>{
+        var res = JSON.parse(response);
+        expect(Array.isArray(res)).to.equal(true);
+      })
+      .then(done)
+      .catch(done)
+  });
+
+  it('getone returns an array of one collection',function(done){
+    postfour()
+      .then(getone)
+      .then(response=>{
+        var res = JSON.parse(response);
+        expect(Array.isArray(res)).to.equal(true);
+      })
+      .then(done)
+      .catch(done)
+  });
+
+  it('gets one collection by date',function(done){
+    postone()
+      .then(posttwo)
+      .then(postfour)
+      .then(getone)
+      .then((response)=>{
+        var res = JSON.parse(response);
+        expect(res.length).to.equal(1);
+        expect(res[0].postings.length).to.equal(1);
+      })
+      .finally(done);
   })
 
-  it('shows nothing after deletion',function(done){
-    //rp.post(server+'/raw-postings',{json: examplePosting1})
-    Promise.all([ rp.delete(server+'/raw-postings/:0'),
-                  rp.get(server+'/raw-postings'), 
-                  ])
-      .then(responses=>{
-        console.log('r1',responses);
-        expect(JSON.parse(responses[1]).length).to.equal(0);
-        done();
-    });
+  it('returns all collections on a getall request',function(done){
+    postone()
+      .then(getall)
+      .then(response=>{
+        var res = JSON.parse(response);
+        expect(res[0].postings[0].country).to.equal('A');
+        
+      })
+      .then(done)
+      .catch(done)
   });
+
 });
 
-
-// describe ('App (using local db)', function() {
-
-//   describe('Posting creation: ', function(done) {
-    
-   
+describe('raw-postings delete request',function(){
   
-    // it('deletes postings', function(done) {
-    //   request(app)
-    //     .delete('/raw-postings/:0')
-    //     .then(function(response){
-    //       //console.log(response.status);
-    //       expect(response.status).to.equal(204);
-    //       done();
-    //     })
-    // });
+  it('deletes postings with statusCode of 204',function(done){
+    deleteall()
+      .on('response',response=>{
+        expect(response.statusCode).to.equal(204);
+        done();
+      });
+  });
 
-    // it('posts postings', function(done) {
-    //   var posts = [request(app).post('/raw-postings').send(examplePosting1),
-    //               request(app).post('/raw-postings').send(examplePosting2),
-    //               request(app).post('/raw-postings').send(examplePosting3)];
+  it('deletes one datelist by date',function(done){
+    postone()
+      .then(posttwo)
+      .then(postfour)
+      .then(deleteone)
+      .then(getall)
+      .then((response)=>{
+        var res = JSON.parse(response);
+        expect(res.length).to.equal(1);
+        expect(res[0].postings[0].country).to.equal('A');
+      })
+      .finally(done);
+  });
 
-    //   Promise.all(posts).then(function(...results){
-    //     console.log(results);
-    //     done();
-    //   });
-    // });
+  it('deletes all posts',function(done){
+    postone()
+      .then(posttwo)
+      .then(deleteall)
+      .then(getall)
+      .then((response)=>{
+        expect(JSON.parse(response).length).to.equal(0);
+      })
+      .finally(done);
+  });
 
-    //  it('gets postings', function(done) {
-    //    request(app)
-    //      .get('/raw-postings?date=0')
-    //      .then(function(response){
-    //      expect(response.status).to.equal(202);
-    //       done();
-    //      });
-    //  });
+});
 
-    //  it('posts', function(done) {
-    //   request(app)
-    //     .post('/raw-postings')
-    //     .send(examplePosting2)
-    //     .then(function(response){
-    //       expect(response.status).to.equal(202);
-    //       expect(response.length).to.equal(1);
-    //       done();
-    //     });
-    //  });
+describe('raw-postings database behavior',function(){
 
-  //}); 
+  it('adds multiple posts of the same date to the same list of postings',function(done){
+    postone()
+      .then(posttwo)
+      .then(getall)
+      .then((response)=>{
+        expect(JSON.parse(response).length).to.equal(1);
+        var res = JSON.parse(response);
+        expect(res[0].postings.length).to.equal(2);
+      })
+      .finally(done);
+  });
 
-  // it('deletes postings', function(done) {
-  //     request(app)
-  //       .delete('/raw-postings/:0')
-  //       .then(function(response){
-  //         console.log(response.status);
-  //         expect(response.status).to.equal(204);
-  //         done();
-  //       })
-//    });
+  it('adds multiple posts of a different date to different lists of postings',function(done){
+    postone()
+      .then(posttwo)
+      .then(postfour)
+      .then(getall)
+      .then((response)=>{
+        var res = JSON.parse(response);
+        expect(res.length).to.equal(2);
+        expect(res[0].postings.length).to.equal(2);
+        expect(res[1].postings.length).to.equal(1);
+      })
+      .finally(done);
+  });
 
-// });
+});
+
+  
+
+
+
