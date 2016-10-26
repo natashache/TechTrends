d22043fcffc8d0bed53765dd03b5fc8c01bf33b0
Aaron Stevens <astev1989@gmail.com>
2016-10-25 20:41:05 -0400

    using factory and controller

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -1,11 +1,25 @@
 angular.module('mainApp', [
 
-]).controller('stateCtrl', ['$scope', function($scope) {
-  $scope.data = {
-    default: 'Select State:',
-    states: Object.keys(keys.collections.geo.united_states)
+])
+.factory('country', function() {
+  var country = keys.collections.geo.united_states;
+  country.states = Object.keys(keys.collections.geo.united_states);
+  return {
+    rootLocations: country
   }
+})
 
-}])
+.controller('stateCtrl', function($scope, country) {
+  $scope.country = country.rootLocations;
+  $scope.states = $scope.country.states;
+  $scope.populateDrop = function() {
+    console.log('Yay, the test worked!');
+  }
+  // $scope.data = {
+  //   default: 'Select State:',
+  //   states: Object.keys(keys.collections.geo.united_states)
+  // }
+
+})
 
 
