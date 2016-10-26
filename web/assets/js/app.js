593515169882566e4cf5499778a5c56fd3c898d5
Aaron Stevens <astev1989@gmail.com>
2016-10-25 22:12:17 -0400

    dropdowns now dynamic / error free

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -13,9 +13,9 @@ angular.module('mainApp', [
 .controller('stateCtrl', function($scope, country) {
   $scope.country = country.rootLocations;
   $scope.states = $scope.country.states;
-  $scope.model = $scope.states[0]
+  $scope.selectedState = $scope.states[0]
   $scope.populateDrop = function() {
-    console.log($scope.model);
+    $scope.currentCities = Object.keys($scope.country[$scope.selectedState]);
   }
 })
 
