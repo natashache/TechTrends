7cdcc4e60931da6a378b77246c30c1ef5e7f2725
Justin <jproche5@gmail.com>
2016-10-28 15:08:49 -0400

    modularize angular code

--- gulpfile.js
+++ gulpfile.js
@@ -52,7 +52,7 @@ gulp.task('watch', function() {
 gulp.task('nodemon', (cb) => {
   let started = false;
   return nodemon({
-    script: 'connections/mlabTestConfig.js',
+    script: 'connections/localhostConfig.js',
   })
     .on('data', () => {
       if (!started) {
