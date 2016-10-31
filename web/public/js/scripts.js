636312e9510e1f898fef4cbb72ef71a8f666146c
Numose <andrew.cernek@gmail.com>
2016-10-31 13:31:23 -0700

    frontend: nav bar now scrolls

--- web/public/js/scripts.js
+++ web/public/js/scripts.js
@@ -40966,3 +40966,22 @@ angular.module('app.controllers', [
 
 
 angular.module('app',['app.controllers','app.services']);
+
+$(function() {
+  var $sidebar   = $("#chart_nav"), 
+    $window    = $(window),
+    offset     = $sidebar.offset(),
+    topPadding = 132;
+
+  $window.scroll(() => {
+    if ($window.scrollTop() > offset.top) {
+        $sidebar.stop().animate({
+            marginTop: $window.scrollTop() - offset.top + topPadding
+        });
+    } else {
+        $sidebar.stop().animate({
+            marginTop: 84
+        });
+    }
+  });
+});
