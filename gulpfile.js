e7d478a9cd668da88a0c3624203075998427ddc6
Justin <jproche5@gmail.com>
2016-10-31 14:29:57 -0400

    gulp nodemon points to mlabTestConfig

--- gulpfile.js
+++ gulpfile.js
@@ -52,7 +52,7 @@ gulp.task('watch', () => {
 gulp.task('nodemon', (cb) => {
   let started = false;
   return nodemon({
-    script: 'connections/localhostConfig.js',
+    script: 'connections/mlabTestConfig.js',
   })
     .on('data', () => {
       if (!started) {
